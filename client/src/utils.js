17d834491c243f15e2fb72b1ae011c9a0e85f41c
ShcoderTarakan <91088551+ShcoderTarakan@users.noreply.github.com>
2022-06-13 15:26:02 +0300

    Delete client directory

--- client/src/utils.js
+++
@@ -1,9 +0,0 @@
-export function scrollToBottom(element) {
-  element.scrollTo(0, element.scrollHeight);
-}
-
-export function formatDate(str) {
-  const date = new Date(str);
-
-  return date.toLocaleString("ru");
-}
